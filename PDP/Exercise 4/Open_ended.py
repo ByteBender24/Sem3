9d7f663da52687270a797353ef0d047ade40b8a8
ByteBender24 <harishrajselva@gmail.com>
2023-10-04 19:38:45 +0530

    Semi_Working: repeating problem of multiple diamond inheritance

--- PDP/Exercise 4/Open_ended.py
+++ PDP/Exercise 4/Open_ended.py
@@ -1,4 +1,6 @@
 '''
+Diamond Inheritance
+
 Create a project that simulates a university's course registration system. There should be a class hierarchy for different types of courses, each with specific properties and methods.Create a base class called Course with a constructor that accepts the course_code, course_name, and 
 credit_hours as positional arguments. Initialize these instance variables in the constructor.Create three derived classes: CoreCourse, ElectiveCourse, and LabCourse, each inheriting from the
 Course class. These derived classes should accept any number of positional arguments (*args) andkeyword arguments (**kwargs) during initialization. Each derived class should override the __init__
@@ -6,6 +8,10 @@ method to add additional properties specific to that type of course.For example,
 courses can have an available_terms property, and LabCourse courses can have a lab_location property. Implement a method called course_info in each derived class that prints information about
 the course, including its code, name, credit hours, and any additional properties specific to that type of course.Create instances of various courses from each category (e.g., a core course, an elective course, and a
 lab course) with different codes, names, credit hours, and additional properties. Demonstrate that each course object can accept different arguments during initialization and correctly display itsinformation using the course_info method.
+
+Modification1 : Code (semi - working)
+Modification2 : Debugging
+Modification3 : Documentation & Formatting
 
 Open-ended question section:
 Extend the code such that it will demonstrate the diamond inheritance property with overriding of
@@ -155,7 +161,43 @@ class CoreCourse(Course):
                 course_info += f"\n{info_key}: {info_value}"
         return course_info
 
+
 class Core_cum_Elective(CoreCourse, ElectiveCourse):
+    """
+    Represents a course that can be both a CoreCourse and an ElectiveCourse. (Diamond_Inheritance)
+
+    This class inherits from CoreCourse and ElectiveCourse, allowing it to have properties and behaviors
+    of both types of courses.
+
+    Parameters:
+    - course_code (str): The unique code of the course.
+    - course_name (str): The name of the course.
+    - credit_hours (int): The credit hours assigned to the course.
+    - **kwargs: Additional keyword arguments for specifying course properties.
+
+    Attributes:
+    - required_prerequisites (list): A list of required prerequisites for the course.
+    - available_terms (list): A list of available terms in which the course can be taken.
+    - other_info (dict): Additional information about the course.
+
+    Methods:
+    - course_info(): Returns a formatted string containing information about the course.
+
+    Example Usage:
+    >>> course = Core_cum_Elective(
+    ...     course_code="CE101",
+    ...     course_name="Advanced Programming",
+    ...     credit_hours=3,
+    ...     required_prerequisites=["CE100"],
+    ...     available_terms=["Fall", "Spring"],
+    ...     other_info={"Instructor": "Dr. Smith", "Location": "Room 101"}
+    ... )
+    >>> print(course.course_info())
+    Title: Advanced Programming, Director: Dr. Smith, Year: 3, Genre: CE101
+    Required Prerequisites: CE100
+    Available Terms: Fall, Spring
+    Location: Room 101
+    """
 
     def __init__(self, course_code, course_name, credit_hours, **kwargs):
         super().__init__(course_code, course_name, credit_hours, **kwargs)
@@ -164,11 +206,17 @@ class Core_cum_Elective(CoreCourse, ElectiveCourse):
         self.other_info = kwargs.get('other_info')
 
     def course_info(self):
+        """
+        Get information about the course.
+
+        Returns:
+        (str): A formatted string containing course information.
+        """
         return super().course_info()
+
 
 if __name__ == "__main__":
 
     Core_cum_Elective1 = Core_cum_Elective("CCE1", "TEE", 79, required_prerequisites=[
                                            "PDP", "Python"], available_terms=["Summer", "Winter"], other_info={"nawa": "name"})
     print(Core_cum_Elective1.course_info())
-
